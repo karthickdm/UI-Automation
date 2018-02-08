75f0cee363013812290d71f253f96744d60403b1
Karthick Dhakshinamoorthy <Karthick_Dhakshinamoorthy@epam.com>
2018-02-08 17:37:36 +0800

    updated shell script

--- runUnittestShell.sh
+++ runUnittestShell.sh
@@ -2,11 +2,5 @@
 
 find . -name "*Test.py" -print | while read f; do
         echo "$f"
-        ###
-        python -m coverage run "$f"
-        python -m coverage xml -o coverage.xml
-        ###
-done
 
-cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
-cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
+done
