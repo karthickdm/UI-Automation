4be08f9d7b0fb4b0c385816676216f03cce67965
Karthick Dhakshinamoorthy <Karthick_Dhakshinamoorthy@epam.com>
2018-02-08 19:25:05 +0800

    test

--- runUnittestShell.sh
+++ runUnittestShell.sh
@@ -2,6 +2,7 @@
 
 #find . -name "*Test.py" -print | while read f; do
 #echo "$f"
-cd $HOME/HKEX/workspace/UI/UI-Automation
+#cd $HOME/HKEX/workspace/UI/UI-Automation
 python -m unittest Script/TrialTest.py
+echo $?
 #done
